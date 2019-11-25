b40992fb9db84ff0a0063d92ceb3c5cdc6400fb7
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2019-11-25 18:40:49 +0200

    Formatting response JSON

--- search.js
+++ search.js
@@ -6,16 +6,20 @@ const Category = require('./model/category');
 
 router.get('/search', async (req, res) => {
   try {
-    const collection = await Category.where({ CategoryName: req.query.keyword }).fetch({ withRelated: ['apps.company.city.country.ggeis'] });
-    const collectionWithEC = await Category.where({ CategoryName: req.query.keyword }).fetch({ withRelated: ['apps.ecs'] });
+    const collection = await Category.where({ CategoryName: req.query.keyword }).fetch({ withRelated: ['apps.company.city.country.ggeis', 'apps.ecs'] });
     const response = [];
     collection.toJSON().apps.forEach((elem) => {
       response.push({
         AppName: elem.AppName,
+        EnergyScore: elem.ecs[0].ConsumptionRate,
+        CompanyScore: elem.company.Carbon_footprint,
+        CityScore: elem.company.city.GGMCF,
+        CountryScore: elem.company.city.country.ggeis[0].Index,
+        TotalScore: null,
       });
     });
+    console.log(collection.toJSON());
     console.log(collection.toJSON().apps);
-    console.log(collectionWithEC.toJSON());
     res.send(response);
   } catch (e) {
     console.log(e);
