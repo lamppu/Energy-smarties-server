b7a8887f41138707426312f2c3e78c40fece8835
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2019-11-28 00:57:18 +0200

    Sorting response, search advanced and code reformatted to more readable form

--- search.js
+++ search.js
@@ -22,7 +22,8 @@ router.get('/search', async (req, res) => {
     const countryScale = await Scaling.where({ ScaleName: 'CountryScale' }).fetch();
     const cityScale = await Scaling.where({ ScaleName: 'CityScale' }).fetch();
     const compScale = await Scaling.where({ ScaleName: 'CompanyScale' }).fetch();
-    let catcollection = true;
+    let partAppName = false;
+    let partCatName = false;
     let collection;
     const getResponse = (elem) => {
       const appName = elem.AppName;
@@ -31,17 +32,23 @@ router.get('/search', async (req, res) => {
       const citycf = elem.company.city.GGMCF;
       const ggei = elem.company.city.country.ggeis[0].Index;
       let es;
-      if (catcollection) {
-        es = score(
-          collection.toJSON().scaling.VariableA,
-          collection.toJSON().scaling.VariableB,
-          rate,
-        );
-      } else {
+      if (partAppName) {
         console.log(elem);
         es = score(
           elem.categories[0].scaling.VariableA,
           elem.categories[0].scaling.VariableB,
+          rate,
+        );
+      } else if (partCatName) {
+        es = score(
+          collection.toJSON()[0].scaling.VariableA,
+          collection.toJSON()[0].scaling.VariableB,
+          rate,
+        );
+      } else {
+        es = score(
+          collection.toJSON().scaling.VariableA,
+          collection.toJSON().scaling.VariableB,
           rate,
         );
       }
@@ -76,35 +83,112 @@ router.get('/search', async (req, res) => {
         TotalScore: totalScore(es, comps, cits, cous).toFixed(one),
       });
     };
-    collection = await Category.where({ CategoryName: req.query.keyword })
-      .fetch({ require: false, withRelated: ['scaling', 'apps.company.city.country.ggeis', 'apps.ecs'] });
+    const sortByTotalScore = (a, b) => {
+      if (parseFloat(a.TotalScore) < parseFloat(b.TotalScore)) {
+        return one;
+      }
+      return -one;
+    };
+    const sortByEnergyScore = (a, b) => {
+      if (parseFloat(a.EnergyScore) < parseFloat(b.EnergyScore)) {
+        return one;
+      }
+      return -one;
+    };
+    const sortByCompanyScore = (a, b) => {
+      if (parseFloat(a.CompanyScore) < parseFloat(b.CompanyScore)) {
+        return one;
+      }
+      return -one;
+    };
+    const sortByCityScore = (a, b) => {
+      if (parseFloat(a.CityScore) < parseFloat(b.CityScore)) {
+        return one;
+      }
+      return -one;
+    };
+    const sortByCountryScore = (a, b) => {
+      if (parseFloat(a.CountryScore) < parseFloat(b.CountryScore)) {
+        return one;
+      }
+      return -one;
+    };
     let response;
-    if (!collection) {
-      collection = await Application.where({ AppName: req.query.keyword })
-        .fetch({ require: false, withRelated: ['categories'] });
-      if (!collection) {
-        collection = await Application.query((qb) => {
-          qb.whereRaw('`AppName` LIKE ?', [`%${req.query.keyword}%`]);
-        }).fetchAll({ require: false, withRelated: ['categories.scaling', 'company.city.country.ggeis', 'ecs'] });
-        if (!collection) {
-          response = ['Nothing found'];
-        } else if (collection.toJSON().length > one) {
-          catcollection = false;
-          response = collection.toJSON().map(getResponse);
-        } else {
-          collection = await Category.where({ id: collection.toJSON().categories[0].id })
-            .fetch({ require: false, withRelated: ['scaling', 'apps.company.city.country.ggeis', 'apps.ecs'] });
-          response = collection.toJSON().apps.map(getResponse);
+    const sortResponse = () => {
+      if (req.query.sortby) {
+        console.log('yes sort');
+        if (req.query.sortby === 'total') {
+          response.sort(sortByTotalScore);
+        } else if (req.query.sortby === 'energy') {
+          response.sort(sortByEnergyScore);
+        } else if (req.query.sortby === 'company') {
+          response.sort(sortByCompanyScore);
+        } else if (req.query.sortby === 'city') {
+          response.sort(sortByCityScore);
+        } else if (req.query.sortby === 'country') {
+          response.sort(sortByCountryScore);
         }
       } else {
-        collection = await Category.where({ id: collection.toJSON().categories[0].id })
-          .fetch({ require: false, withRelated: ['scaling', 'apps.company.city.country.ggeis', 'apps.ecs'] });
-        response = collection.toJSON().apps.map(getResponse);
+        response.sort(sortByTotalScore);
       }
-    } else {
+    };
+    // We start the search here by looking for exact match from category
+    collection = await Category.where({ CategoryName: req.query.keyword })
+      .fetch({ require: false, withRelated: ['scaling', 'apps.company.city.country.ggeis', 'apps.ecs'] });
+    if (collection) {
+      // Exact match found from category
       response = collection.toJSON().apps.map(getResponse);
+      sortResponse();
+      res.send(response);
     }
-    res.send(response);
+    // No exact category was found, continue by looking for exact match from apps
+    collection = await Application.where({ AppName: req.query.keyword })
+      .fetch({ require: false, withRelated: ['categories'] });
+    if (collection) {
+      // Exact match was found from apps
+      collection = await Category.where({ id: collection.toJSON().categories[0].id })
+        .fetch({ require: false, withRelated: ['scaling', 'apps.company.city.country.ggeis', 'apps.ecs'] });
+      response = collection.toJSON().apps.map(getResponse);
+      sortResponse();
+      let toBeUnShifted;
+      response = response.filter((elem) => {
+        if (elem.AppName.toLowerCase() === req.query.keyword) {
+          toBeUnShifted = elem;
+          return false;
+        }
+        return true;
+      });
+      response.unshift(toBeUnShifted);
+      res.send(response);
+    }
+    // No exact app was found, continue by looking for apps
+    // for which the app name contains the search keyword
+    collection = await Application.query((qb) => {
+      qb.whereRaw('`AppName` LIKE ?', [`%${req.query.keyword}%`]);
+    }).fetchAll({ require: false, withRelated: ['categories.scaling', 'company.city.country.ggeis', 'ecs'] });
+    if (collection.length) {
+      // Found app/apps
+      partAppName = true;
+      response = collection.toJSON().map(getResponse);
+      sortResponse();
+      res.send(response);
+    }
+    // No apps were found, continue by looking for categories
+    // for which the category name contains the search keyword.
+    // NOT HANDLED: What if the search finds multiple categories?
+    collection = await Category.query((qb) => {
+      qb.whereRaw('`CategoryName` LIKE ?', [`%${req.query.keyword}%`]);
+    }).fetchAll({ require: false, withRelated: ['scaling', 'apps.company.city.country.ggeis', 'apps.ecs'] });
+    if (collection.length) {
+      partCatName = true;
+      console.log(collection.toJSON());
+      response = collection.toJSON()[0].apps.map(getResponse);
+      sortResponse();
+      res.send(response);
+    } else {
+      response = ['Nothing found'];
+      res.send(response);
+    }
   } catch (e) {
     console.log(e);
     res.send(['Nothing found']);
